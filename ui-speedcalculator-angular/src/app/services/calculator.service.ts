5c1cea8a18a3b361f544790122df414b82ccc2ba
robbmoskv <fabrizio_39@hotmail.com>
2021-05-04 09:48:52 +0200

    Change logic to split h/m/s

--- ui-speedcalculator-angular/src/app/services/calculator.service.ts
+++ ui-speedcalculator-angular/src/app/services/calculator.service.ts
@@ -5,22 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class CalculatorService {
 
-  private travelTime: number;
+  private travelTime: string;
 
   constructor() { }
 
-  calculateTravelTime(speed: number, distance: number){
-    // Speed (m/s) = travel time (s) / distance (m)
+  calculateTravelTime(speed: number, distance: number) {
     // s = v · t + s0 -> t = s / v
+    // traveltime (t) = distance (m) / speed (m/s)
 
     // Speed in meter/seconds
-    let v = (speed * 1000) / 3600;
+    let s = (speed * 1000) / 3600;
     // Distance in meter
-    let s = distance * 1000;
+    let d = distance * 1000;
     // Travel time in minutes
-    this.travelTime = (s / v) / 60;
+    let t = (d / s);
 
-    // this.travelTime = (Math.round((s / v) / 60)).toFixed(2);
+    this.travelTime = this.getTimeStamp(t);
+
     return this.travelTime;
   }
+
+  private getTimeStamp(raw: number) {
+
+    //let ts = seconds;
+    let h = Math.floor(raw / 3600);
+    raw %= 3600;
+
+    let m = Math.floor(raw / 60);
+    let s = Math.floor(raw % 60);
+
+    // If you want strings with leading zeroes:
+    let result = String(h).padStart(2, "0") + "h:" + String(m).padStart(2, "0") +  "m:" + String(s).padStart(2, "0") + "s";
+
+    return result;
+  }
+
 }
